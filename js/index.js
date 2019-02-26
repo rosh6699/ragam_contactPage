22ada076a3dcd412a18b4ecef54fa78680d2f015
Roshan R <rosh6699@gmail.com>
2019-02-26 22:25:58 +0530

    js changed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#btn-all").click(function() {
+    //event.preventDefault();
     $("html, body").animate(
       {
         scrollTop: $("#scrollTo").offset().top
